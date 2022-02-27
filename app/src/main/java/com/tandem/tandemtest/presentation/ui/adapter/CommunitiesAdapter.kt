4ba2392a495da07006f4bb2c7dcf8994f50f539e
majidsoft <12241368>
2022-02-27 19:19:27 +0330

    implement values( dimens,styles,strings,colors)

--- app/src/main/java/com/tandem/tandemtest/presentation/ui/adapter/CommunitiesAdapter.kt
+++ app/src/main/java/com/tandem/tandemtest/presentation/ui/adapter/CommunitiesAdapter.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
 import com.tandem.tandemtest.R
+import com.tandem.tandemtest.core.extension.hide
+import com.tandem.tandemtest.core.extension.show
 import com.tandem.tandemtest.core.imageloader.IImageLoader
 import com.tandem.tandemtest.presentation.model.Community
 import kotlinx.android.synthetic.main.community_row.view.*
@@ -58,12 +60,30 @@ class CommunitiesAdapter(
     inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         fun bind(community: Community) {
             with(community) {
+
                 view.firstNameTextView.text = firstName
                 view.topicTextView.text = topic
-                view.referenceCountTextView.text = referenceCnt.toString()
-                view.learnTextView.text = learns[0]
-                view.nativeTextView.text = natives[0]
+                view.learnTextView.text = when {
+                    learns.size > 1 -> learns.first() + " +" + (learns.size - 1).toString()
+                    learns.size == 1 -> learns.first()
+                    else -> ""
+                }
+                view.nativeTextView.text = when {
+                    natives.size > 1 -> natives.first() + " +" + (natives.size - 1).toString()
+                    natives.size == 1 -> natives.first()
+                    else -> ""
+                }
                 view.tag = this
+
+
+                if (referenceCnt == 0) {
+                    view.referenceCountTextView.hide()
+                    view.newIconImageView.show()
+                } else {
+                    view.referenceCountTextView.show()
+                    view.newIconImageView.hide()
+                    view.referenceCountTextView.text = referenceCnt.toString()
+                }
 
                 if (isLike) {
                     view.likeImageView.setImageDrawable(
